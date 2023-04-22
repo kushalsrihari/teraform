5315d202530a73ac89c975eda3442aee3bddab8e
kushal <kushalsrihari@gmail.com>
2023-04-22 19:10:26 +0530

    elastic bean

---
+++ bean-env.tf
@@ -0,0 +1,124 @@
+resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
+  name = "vprofile-bean-prod"
+  application = aws_elastic_beanstalk_application.vprofile-prod
+  solution_stack_name = "64bit Amzon Linux 2 v4.1.1 running Tomcat 8.5 Corretto 11"
+  cname_prefix = "vprofile-bean-prod-domain123321"
+  setting {
+    name      = "VPCId"
+    namespace = "aws:ec2:vpc"
+    value     = module.vpc.vpc_id
+  }
+  setting {
+    name      = "IamInstanceProfile"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    name      = "AssociatePublicIpAddress"
+    namespace = "aws:ec2:vpc"
+    value     = "false"
+  }
+  setting {
+    name      = "aws:ec2:vpc"
+    namespace = "Subnets"
+    value     = join(",",[module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]])
+  }
+  setting {
+    name      = "ELBSubnets"
+    namespace = "aws:ec2:vpc"
+    value     = join(",",[module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]])
+  }
+  setting {
+    name      = "InstanceType"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value     = "t2.micro"
+  }
+  setting {
+    namespace      = "aws:autoscaling:launchconfiguration"
+    name = "EC2KeyName"
+    value     = aws_key_pair.vprofilekey.key_name
+  }
+  setting {
+    namespace      = "aws:autoscaling:asg"
+    name = "Availability Zones"
+    value     = "Any 3"
+  }
+  setting {
+    namespace      = "aws:autoscaling:asg"
+    name = "MinSize"
+    value     = "1"
+  }
+  setting {
+    namespace      = "aws:autoscaling:asg"
+    name = "MaxSize"
+    value     = "8"
+  }
+  setting {
+    namespace      = "aws:elasticbeanstalk:application:environment"
+    name = "environment"
+    value     = "prod"
+  }
+  setting {
+    namespace      = "LOGGING_APPENDER"
+    name = "aws:elasticbeanstalk:application:environment"
+    value     = "GRAYLOG"
+  }
+  setting {
+    name      = "SystemType"
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    value     = "enhanced"
+  }
+  setting {
+    name      = "RollingUpdateEnabled"
+    namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+    value     = "true"
+  }
+  setting {
+    name      = "RollingUpdateType"
+    namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+    value     = "Health"
+  }
+  setting {
+    name      = "MaxBatchSize"
+    namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+    value     = "1"
+  }
+  setting {
+    name      = "CrossZone"
+    namespace = "aws:elb:loadblancer"
+    value     = "true"
+  }
+  setting {
+    name      = "BatchSizeType"
+    namespace = "aws:elasticbeanstalk:command"
+    value     = "Fixed"
+  }
+  setting {
+    name      = "BatchSize"
+    namespace = "aws:elasticbeanstalk:command"
+    value     = "1"
+  }
+  setting {
+    name      = "StickinessEnables"
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    value     = "true"
+  }
+  setting {
+    name      = "DeploymentPolicy"
+    namespace = "aws:elasticbeanstalk:command"
+    value     = "Rolling"
+  }
+  setting {
+    name      = "SecurityGroups"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value     = aws_security_group.vprofile-prod-sg.id
+  }
+  setting {
+    name      = "SecurityGroups"
+    namespace = "aws:elbv2:launchconfiguration"
+    value     = aws_security_group.vprofile-bean-elb-sg.id
+  }
+
+  depends_on = [aws_security_group.vprofile-bean-elb-sg, aws_security_group.vprofile-prod-sg]
+
+}
