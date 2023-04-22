f72ee2e0c9aec43ff88b6fb6c4ca6e3288b08bb4
kushal <kushalsrihari@gmail.com>
2023-04-22 08:39:51 +0530

    VPC

--- Backend-s3.tf
+++ Backend-s3.tf
@@ -1,7 +1,7 @@
-terraform{
+terraform {
   backend "s3" {
     bucket = "terra-vprofile-1234"
-    key = "terraform/backend"
+    key    = "terraform/backend"
     region = "us-east-1"
   }
 }
--- keypairs.tf
+++ keypairs.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "vprofile" {
-  key_name = "vprofile"
+  key_name   = "vprofile"
   public_key = file(var.PUB_KEY_PATH)
 }
