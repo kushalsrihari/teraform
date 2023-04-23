77586373ce7b9d48eefe107852177b5f4138993e
kushal <kushalsrihari@gmail.com>
2023-04-23 15:34:31 +0530

    elastic bean

--- bean-env.tf
+++ bean-env.tf
@@ -1,8 +1,8 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
-  name = "vprofile-bean-prod"
-  application = aws_elastic_beanstalk_application.vprofile-prod
-  solution_stack_name = "64bit Amzon Linux 2 v4.1.1 running Tomcat 8.5 Corretto 11"
-  cname_prefix = "vprofile-bean-prod-domain123321"
+  name                = "vprofile-bean-prod"
+  application         = aws_elastic_beanstalk_application.vprofile-prod.name
+  solution_stack_name = "64bit Amazon Linux 2 v4.3.6 running Tomcat 8.5 Corretto 11"
+  cname_prefix        = "vprofile-bean-prod-domain123321"
   setting {
     name      = "VPCId"
     namespace = "aws:ec2:vpc"
@@ -21,12 +21,12 @@ resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   setting {
     name      = "aws:ec2:vpc"
     namespace = "Subnets"
-    value     = join(",",[module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]])
+    value     = join(",", [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]])
   }
   setting {
     name      = "ELBSubnets"
     namespace = "aws:ec2:vpc"
-    value     = join(",",[module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]])
+    value     = join(",", [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]])
   }
   setting {
     name      = "InstanceType"
@@ -34,33 +34,33 @@ resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
     value     = "t2.micro"
   }
   setting {
-    namespace      = "aws:autoscaling:launchconfiguration"
-    name = "EC2KeyName"
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "EC2KeyName"
     value     = aws_key_pair.vprofile.key_name
   }
   setting {
-    namespace      = "aws:autoscaling:asg"
-    name = "Availability Zones"
+    namespace = "aws:autoscaling:asg"
+    name      = "Availability Zones"
     value     = "Any 3"
   }
   setting {
-    namespace      = "aws:autoscaling:asg"
-    name = "MinSize"
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
     value     = "1"
   }
   setting {
-    namespace      = "aws:autoscaling:asg"
-    name = "MaxSize"
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
     value     = "8"
   }
   setting {
-    namespace      = "aws:elasticbeanstalk:application:environment"
-    name = "environment"
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "environment"
     value     = "prod"
   }
   setting {
-    namespace      = "LOGGING_APPENDER"
-    name = "aws:elasticbeanstalk:application:environment"
+    namespace = "LOGGING_APPENDER"
+    name      = "aws:elasticbeanstalk:application:environment"
     value     = "GRAYLOG"
   }
   setting {
